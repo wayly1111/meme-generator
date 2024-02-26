13a8b9ec4b8163e8b6109020dcee543c25825bbf
wayly1111 <wayly00@mail.ru>
2024-02-26 17:38:08 +0300

    added text on image

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from PIL import Image, ImageDraw, ImageFont
 print("Генератор мемов запущен!")
 top_text = input("Введите верхний текст: ")
 bottom_text = input("Введите нижний текст: ")
@@ -14,3 +15,12 @@ if image_number == 1:
 elif image_number == 2:
     image_file = "images/cat_in_glasses.png"
 print(image_file)
+
+image = Image.open(image_file)
+draw = ImageDraw.Draw(image)
+font = ImageFont.truetype("arial.ttf", size=70)
+
+draw.text((0, 0), top_text, font=font, fill="blue")
+draw.text((0, 100), bottom_text, font=font, fill="blue")
+
+image.save("meme.jpg")
