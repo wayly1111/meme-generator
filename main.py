94f58992da645a2e3bc54b8a65f264a9d1d95a85
wayly1111 <wayly00@mail.ru>
2024-03-04 17:31:53 +0300

    finished meme generator

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from PIL import Image, ImageDraw, ImageFont
+
 print("Генератор мемов запущен!")
 top_text = input("Введите верхний текст: ")
 bottom_text = input("Введите нижний текст: ")
@@ -17,10 +18,22 @@ elif image_number == 2:
 print(image_file)
 
 image = Image.open(image_file)
+width, height = image.size
 draw = ImageDraw.Draw(image)
 font = ImageFont.truetype("arial.ttf", size=70)
 
-draw.text((0, 0), top_text, font=font, fill="blue")
-draw.text((0, 100), bottom_text, font=font, fill="blue")
+text_size = draw.textbbox((0, 0), top_text, font)
+text_width = text_size[2]
+
+draw.text(((width - text_width) / 2, 10), top_text, font=font, fill="blue")
+
+text_size = draw.textbbox((0, 0), bottom_text, font)
+text_width = text_size[2]
+text_height = text_size[3]
+
+#draw.text((0, 0), top_text, font=font, fill="blue")
+#draw.text((0, 100), bottom_text, font=font, fill="blue")
+
+draw.text(((width - text_width) / 2, height - text_height - 10), bottom_text, font=font, fill="blue")
 
 image.save("meme.jpg")
